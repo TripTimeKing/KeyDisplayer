60a21220c81872d3fa1b676d9aa4fa096ce1d1e9
TripTimeKing <ali.krol@gmail.com>
2023-07-19 20:59:10 +0200

    Finishing up before adding .exe functionality

--- Main.py
+++ Main.py
@@ -1,13 +1,30 @@
 from pynput.keyboard import Key, Listener
+from keyboard import read_key
+from pynput.mouse import Listener as mouseListener
+from pynput.mouse import Button
+keyLog = []
+
  
 def show(key):
- 
-    print('\nYou Entered {0}'.format( key))
- 
-    if key == Key.delete:
-        # Stop listener
-        return False
- 
-# Collect all event until released
-with Listener(on_press = show) as listener:  
-    listener.join()
+    print('\nYou Entered {0}'.format(key))
+    keyLog.append(key)
+    
+def clicked(x,y, button, pressed):
+    if pressed == False: return
+    print('\nYou Clicked {0}'.format(button))
+    keyLog.append(button)
+    
+def main():
+    listener_keyboard = Listener(on_press=show)
+    listener_mouse = mouseListener(on_click=clicked)
+    listener_mouse.start()
+    listener_keyboard.start()
+
+    # temp solution for running the program
+    while True:
+        if read_key() == "delete":
+            print(keyLog)
+            quit()
+            
+if __name__ == "__main__":
+    main()
